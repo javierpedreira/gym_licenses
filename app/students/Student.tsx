1dd0a7af278e1f9b27ca4354603f3afbf3f09cb8
Javier Pedreira <javier.pedreira@intenthq.com>
2022-11-27 12:14:16 +0100

    Adds ability to edit students

--- app/students/Student.tsx
+++ app/students/Student.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import {useState} from 'react'
+
 export interface Student {
   id: string
   name: string
@@ -22,7 +24,31 @@ async function deleteStudent(id: string) {
   return res
 }
 
+const body = (name: string, email: string, birthday: string) => {
+  return JSON.stringify({
+    name,
+    email,
+    birthday
+  })
+}
+
+async function editStudent(id: string, name: string, email: string, birthday: string) {
+  const res = await fetch(`http://127.0.0.1:8090/api/collections/students/records/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: body(name, email, birthday)
+  })
+
+  return res
+}
+
 export default function StudentCompoent({name, email, birthday, id, updateStudents}: StudentProps) {
+  const [newBirthday, setBirthday] = useState(birthday)
+  const [newName, setName] = useState(name)
+  const [newEmail, setEmail] = useState(email)
+
   const callDelete = () => {
     deleteStudent(id)
       .then(() => {
@@ -33,12 +59,33 @@ export default function StudentCompoent({name, email, birthday, id, updateStuden
       })
   }
 
+  const callUpdate = () => {
+    editStudent(id, newName, newEmail, newBirthday)
+      .then(() => {
+        updateStudents()
+      })
+      .catch((error) => {
+        console.log(`Something went wrong, ${error}`)
+      })
+  }
+
   return (
-    <>
-      <div>Name: {name}</div>
-      <div>Email: {email}</div>
-      <div>Birthday: {birthday}</div>
+    <form onSubmit={callUpdate}>
+      <label htmlFor="html">Name:</label>
+      <input type="text" placeholder="Name" value={newName} onChange={(e) => setName(e.target.value)} />
+      <label htmlFor="html">Email:</label>
+      <input type="email" placeholder="Email" value={newEmail} onChange={(e) => setEmail(e.target.value)} />
+      <label htmlFor="html">Birhday:</label>
+      <input
+        type="date"
+        placeholder="Birthday"
+        value={newBirthday.slice(0, 10)}
+        onChange={(e) => setBirthday(e.target.value)}
+      />
       <button onClick={callDelete}>Delete</button>
-    </>
+      <button type="submit" onClick={callUpdate}>
+        UpdateStudent
+      </button>
+    </form>
   )
 }
