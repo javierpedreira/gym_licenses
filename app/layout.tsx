d04eecb57bd440d71c0de198e09c6e78539148b6
Javier Pedreira <javier.pedreira@intenthq.com>
2022-12-03 17:35:54 +0100

    Adds user authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
+'use client'
+import {createBrowserSupabaseClient} from '@supabase/auth-helpers-nextjs'
+import {SessionContextProvider, Session} from '@supabase/auth-helpers-react'
+import {AppProps} from 'next/app'
+import {useState} from 'react'
+
 export default function RootLayout({children}: {children: React.ReactNode}) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <SessionContextProvider supabaseClient={supabase}>{children}</SessionContextProvider>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-export default function App() {
+'use client'
+
+import {Auth, ThemeSupa} from '@supabase/auth-ui-react'
+import {useSession, useSupabaseClient} from '@supabase/auth-helpers-react'
+import Account from './components/Account'
+
+export default function Home() {
+  const session = useSession()
+  const supabase = useSupabaseClient()
+
   return (
-    <>
-      <div>Hola Manolo</div>
-    </>
+    <div className="container" style={{padding: '50px 0 100px 0'}}>
+      {!session ? (
+        <Auth supabaseClient={supabase} appearance={{theme: ThemeSupa}} theme="dark" />
+      ) : (
+        <Account session={session} />
+      )}
+    </div>
   )
 }
