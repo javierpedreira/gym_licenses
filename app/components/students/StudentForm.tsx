57f66b63199ac03eb8a6abbb0e38f89600bb5366
Javier Pedreira <javier.pedreira@intenthq.com>
2022-12-06 16:11:19 +0100

    Fixes Student profile layour

--- app/components/students/StudentForm.tsx
+++ app/components/students/StudentForm.tsx
@@ -41,56 +41,52 @@ export default function StudentForm({name, email, birthday, id}: StudentProps) {
   }
 
   return (
-    <form className="py-10">
-      <div className="bg-slate-300 w-80 py-10 rounded-lg">
-        <div className="p-2 flex">
-          <label className="font-bold px-2 flex-1" htmlFor="html">
-            Name:
-          </label>
-          <input
-            className="px-4 py-1 mr-4 flex-2 rounded-lg"
-            type="text"
-            placeholder="Name"
-            value={newName}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="p-2 flex">
-          <label className="font-bold px-2 flex-1" htmlFor="html">
-            Email:
-          </label>
-          <input
-            className="px-4 py-2 mr-4 flex-2 rounded-lg"
-            type="email"
-            placeholder="Email"
-            value={newEmail}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="p-2 flex">
-          <label className="font-bold px-2 flex-1" htmlFor="html">
-            Birhday:
-          </label>
-          <input
-            className="align-right px-4 py-2 mr-4 flex-2 rounded-lg"
-            type="date"
-            placeholder="Birthday"
-            value={newBirthday.slice(0, 10)}
-            onChange={(e) => setBirthday(e.target.value)}
-          />
-        </div>
-        <div className="flex p-2">
-          <button
-            className="bg-slate-800 hover:bg-slate-600 text-slate-100 px-4 py-2 mr-4 font-bold flex-auto rounded-lg"
-            type="submit"
-            onClick={callUpdate}>
-            Actualizar
-          </button>
-          <button className="px-4 py-2 mr-4 font-bold hover:text-slate-600 flex-auto rounded-lg" onClick={callDelete}>
-            Borrar
-          </button>
-        </div>
+    <form className="flex my-10 p-5 bg-slate-300 rounded-lg">
+      <div className="flex-auto">
+        <label className="font-bold px-2 flex-1" htmlFor="html">
+          Nombre:
+        </label>
+        <input
+          className="px-4 py-2 mr-4 flex-2 rounded-lg"
+          type="text"
+          placeholder="Name"
+          value={newName}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
+      <div className="flex-auto">
+        <label className="font-bold px-2 flex-1" htmlFor="html">
+          Email:
+        </label>
+        <input
+          className="px-4 py-2 mr-4 flex-2 rounded-lg"
+          type="email"
+          placeholder="Email"
+          value={newEmail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="flex-auto">
+        <label className="font-bold px-2 flex-1" htmlFor="html">
+          Fecha de nacimiento:
+        </label>
+        <input
+          className="align-right px-4 py-2 mr-4 flex-2 rounded-lg"
+          type="date"
+          placeholder="Birthday"
+          value={newBirthday.slice(0, 10)}
+          onChange={(e) => setBirthday(e.target.value)}
+        />
+      </div>
+      <button
+        className="bg-slate-800 hover:bg-slate-600 text-slate-100 px-4 py-2 mr-4 font-bold flex-auto rounded-lg"
+        type="submit"
+        onClick={callUpdate}>
+        Actualizar
+      </button>
+      <button className="px-4 py-2 mr-4 font-bold hover:text-slate-600 flex-1 rounded-lg" onClick={callDelete}>
+        Borrar
+      </button>
     </form>
   )
 }
