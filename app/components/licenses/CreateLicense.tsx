4088c9f40afdf4b5299bb67d222b9bf459393d61
Javier Pedreira <javier.pedreira@intenthq.com>
2022-12-09 10:37:49 +0100

    Prevents licenses creation when the date or identifiers are wrong

--- app/components/licenses/CreateLicense.tsx
+++ app/components/licenses/CreateLicense.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {useState} from 'react'
+import {ChangeEvent, useState} from 'react'
 import {LicensesService} from '../../dbOps/LicensesService'
 
 const TODAY = new Date().toISOString().slice(0, 10)
@@ -12,13 +12,19 @@ interface CreateLicenseProps {
 export default function CreateLicense({owner}: CreateLicenseProps) {
   const [expedition, setExpeditionDate] = useState(TODAY)
   const [expiration, setExpirationDate] = useState(TODAY)
-  const [identifier, setIdentifier] = useState(0)
+  const [identifier, setIdentifier] = useState('')
 
   const create = async () => {
-    LicensesService.create({identifier, expedition, owner: owner, expiration})
+    LicensesService.create({identifier: Number(identifier), expedition, owner: owner, expiration})
 
     setExpeditionDate(TODAY)
-    setIdentifier(0)
+    setIdentifier('')
+  }
+
+  const canCreateLicense = (): boolean | undefined => {
+    const d1 = new Date(expedition)
+    const d2 = new Date(expiration)
+    return !!identifier && !!!Number.isNaN(Number(identifier)) && d2 >= d1
   }
 
   return (
@@ -26,10 +32,10 @@ export default function CreateLicense({owner}: CreateLicenseProps) {
       <div className="flex shadow sm:overflow-hidden sm:rounded-md">
         <input
           className="px-4 py-2 mr-4 flex-auto rounded-lg"
-          type="number"
+          type="string"
           placeholder="Identifier"
           value={identifier}
-          onChange={(e) => setIdentifier(e.target.valueAsNumber)}
+          onChange={(e) => setIdentifier(e.target.value)}
         />
         <input
           className="px-4 py-2 mr-4 flex-auto rounded-lg"
@@ -46,9 +52,9 @@ export default function CreateLicense({owner}: CreateLicenseProps) {
           onChange={(e) => setExpirationDate(e.target.value)}
         />
         <button
-          className="px-6 bg-neutral-500 rounded-lg font-bold text-zinc-200 hover:bg-neutral-400"
+          className="px-6 bg-neutral-500 rounded-lg font-bold text-zinc-200 hover:bg-neutral-400 disabled:bg-neutral-400"
           type="submit"
-          disabled={!identifier}>
+          disabled={!canCreateLicense()}>
           Agregar
         </button>
       </div>
